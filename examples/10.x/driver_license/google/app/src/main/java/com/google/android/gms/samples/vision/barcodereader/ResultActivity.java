1dc218bdfa31db318a584444e9c8f6654550b593
yushulx <lingxiao1002@gmail.com>
2025-12-29 14:32:57 +0800

    Updated driver_license/google/

--- examples/10.x/driver_license/google/app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ResultActivity.java
+++ examples/10.x/driver_license/google/app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ResultActivity.java
@@ -2,8 +2,8 @@ package com.google.android.gms.samples.vision.barcodereader;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
